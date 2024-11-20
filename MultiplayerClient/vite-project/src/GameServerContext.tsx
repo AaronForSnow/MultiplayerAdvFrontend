302c730f68880d4fb11ace87fe421a8311874fea
aaronforsnow <twodoschool@gmail.com>
2024-11-20 11:33:05 -0700

    just after algorithms

--- MultiplayerClient/vite-project/src/GameServerContext.tsx
+++ MultiplayerClient/vite-project/src/GameServerContext.tsx
@@ -1,12 +1,24 @@
 import { createContext, useContext } from "react";
 import { PlayerVehicle } from "./interfaces/PlayerVehicle";
-
-
+ 
+export interface VehicleFlags {
+  id: number,
+  vehicleAction:
+           | "moveForward"    // 'w' pressed
+           | "moveBackward"   // 's' pressed
+           | "turnLeft"       // 'a' pressed
+           | "turnRight"      // 'd' pressed
+           | "stopForwards"   // when user lets go of 'w' key
+           | "stopBackwards"  // when user lets go of 's' key
+           | "stopLeft"       // when user lets go of 'a' key
+           | "stopRight"
+}
+ 
 export interface VehicleContextInterface {
   vehicle: PlayerVehicle;
-  setVehicleFlags: (setVehicleFlags: void) => void;
+  setVehicleFlags: (setVehicleFlags: VehicleFlags) => void;
 }
-
+ 
 export const GameServerContextContext = createContext<VehicleContextInterface>({
     vehicle: {
         id: 0,
@@ -19,10 +31,11 @@ export const GameServerContextContext = createContext<VehicleContextInterface>({
         moveForward: false,
         moveBackward: false
     },
-    setVehicleFlags: (setVehicleFlags: void) => {},//returns nothing, updates vehicle
+    setVehicleFlags: (setVehicleFlags: VehicleFlags) => {},//returns nothing, updates vehicle
 });
-
-
+ 
+ 
 export const useVehicleContext = () => {
   return useContext(GameServerContextContext)
 }
+ 
--- MultiplayerClient/vite-project/src/GameServerContextProvider.tsx
+++ MultiplayerClient/vite-project/src/GameServerContextProvider.tsx
@@ -1,42 +1,33 @@
 import { FC, ReactNode, useEffect, useState } from "react";
-import { GameServerContextContext } from "./GameServerContext";
+import { GameServerContextContext, VehicleFlags } from "./GameServerContext";
 import { PlayerVehicle } from "./interfaces/PlayerVehicle";
-
+ 
 export const GameServerContextProvider: FC<{ children: ReactNode }> = ({
     children,
   }) => {
     const [vehicle, setVehicle] = useState<PlayerVehicle>();
     const setVehicleFlags = (
-        id: number,
-        vehicleAction:
-           | "moveForward"    // 'w' pressed
-           | "moveBackward"   // 's' pressed
-           | "turnLeft"       // 'a' pressed
-           | "turnRight"      // 'd' pressed
-           | "stopForwards"   // when user lets go of 'w' key
-           | "stopBackwards"  // when user lets go of 's' key
-           | "stopLeft"       // when user lets go of 'a' key
-           | "stopRight"      // when user lets go of 'd' key
-     ) => {
-        switch(vehicleAction){
+     flags: VehicleFlags    
+    ) => {
+        switch(flags.vehicleAction){
             case "moveForward":
-                
-                break;
+              setVehicle({ , moveForward: true });
+              break;
         }
      }
-  
+ 
     useEffect(() => {
-      
+     
     }, []);
-  
+ 
     return (
       <GameServerContextContext.Provider
         value={{
-          vehicle: vehicle,
+          vehicle: vehicle!,
+          setVehicleFlags: setVehicleFlags
         }}
       >
         {children}
       </GameServerContextContext.Provider>
     );
   };
-  
